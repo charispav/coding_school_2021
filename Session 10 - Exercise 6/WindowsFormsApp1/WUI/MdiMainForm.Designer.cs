f6a6a9e694a233c41fa96a9fa0db52a8217a3987
Charalampos Pavlidis <pavlidis.charis@gmail.com>
2021-06-14 13:44:34 +0300

    edit: mdi form included

---
+++ Session 10 - Exercise 6/WindowsFormsApp1/WUI/MdiMainForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WindowsFormsApp1.WUI {
+    partial class MdiMainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newCourseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.coursesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.professorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.professorsToolStripMenuItem,
+            this.coursesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(990, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newCourseToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.fileToolStripMenuItem.Text = "University";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // newCourseToolStripMenuItem
+            // 
+            this.newCourseToolStripMenuItem.Name = "newCourseToolStripMenuItem";
+            this.newCourseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newCourseToolStripMenuItem.Text = "Load...";
+            this.newCourseToolStripMenuItem.Click += new System.EventHandler(this.newCourseToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.studentsToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.viewToolStripMenuItem1});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.viewToolStripMenuItem.Text = "Students";
+            // 
+            // studentsToolStripMenuItem
+            // 
+            this.studentsToolStripMenuItem.Name = "studentsToolStripMenuItem";
+            this.studentsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.studentsToolStripMenuItem.Text = "Add...";
+            this.studentsToolStripMenuItem.Click += new System.EventHandler(this.studentsToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem1
+            // 
+            this.viewToolStripMenuItem1.Name = "viewToolStripMenuItem1";
+            this.viewToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.viewToolStripMenuItem1.Text = "View";
+            this.viewToolStripMenuItem1.Click += new System.EventHandler(this.viewToolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // coursesToolStripMenuItem
+            // 
+            this.coursesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem1,
+            this.viewToolStripMenuItem3});
+            this.coursesToolStripMenuItem.Name = "coursesToolStripMenuItem";
+            this.coursesToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.coursesToolStripMenuItem.Text = "Courses";
+            // 
+            // professorsToolStripMenuItem
+            // 
+            this.professorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.viewToolStripMenuItem2});
+            this.professorsToolStripMenuItem.Name = "professorsToolStripMenuItem";
+            this.professorsToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.professorsToolStripMenuItem.Text = "Professors";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addToolStripMenuItem.Text = "Add...";
+            // 
+            // viewToolStripMenuItem2
+            // 
+            this.viewToolStripMenuItem2.Name = "viewToolStripMenuItem2";
+            this.viewToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.viewToolStripMenuItem2.Text = "View";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save...";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // addToolStripMenuItem1
+            // 
+            this.addToolStripMenuItem1.Name = "addToolStripMenuItem1";
+            this.addToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.addToolStripMenuItem1.Text = "Add...";
+            this.addToolStripMenuItem1.Click += new System.EventHandler(this.addToolStripMenuItem1_Click);
+            // 
+            // viewToolStripMenuItem3
+            // 
+            this.viewToolStripMenuItem3.Name = "viewToolStripMenuItem3";
+            this.viewToolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.viewToolStripMenuItem3.Text = "View";
+            this.viewToolStripMenuItem3.Click += new System.EventHandler(this.viewToolStripMenuItem3_Click);
+            // 
+            // MdiMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 610);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MdiMainForm";
+            this.Text = "MdiMainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MdiMainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newCourseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem professorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem coursesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem3;
+    }
+}
---
+++ Session 10 - Exercise 6/WindowsFormsApp1/WUI/MdiMainForm.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using WindowsFormsApp1.Impl;
+
+namespace WindowsFormsApp1.WUI {
+
+
+    public partial class MdiMainForm : Form {
+
+
+        private const string _LogFile = "Log.txt";
+        private const string _TxtFile = "UniversityData.txt";
+        private const string _JsonFile = "UniversityData.json";
+
+        // ..
+        private University CodingSchool; // = new University(); 
+
+        public MdiMainForm() {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e) {
+
+            MainForm f = new MainForm();
+
+            f.MdiParent = this;
+
+            f.Show();
+
+        }
+
+        private void studentsToolStripMenuItem_Click(object sender, EventArgs e) {
+
+            AddStudent();
+
+
+        }
+
+
+        private void AddStudent() {
+            Student student = new Student();
+
+            StudentForm form = new StudentForm();
+            form.NewStudent = student;
+
+            DialogResult result = form.ShowDialog();
+            switch (result) {
+                case DialogResult.OK:
+                    CodingSchool.Students.Add(student);
+                    break;
+
+                default:
+                    // messagge  ?
+                    break;
+            }
+        }
+
+
+        private void AddCourse() {
+            Course course = new Course();
+
+            CourseForm form = new CourseForm();
+            form.NewCourse = course;
+
+            DialogResult result = form.ShowDialog();
+            switch (result) {
+                case DialogResult.OK:
+                    CodingSchool.Courses.Add(course);
+                    break;
+
+                default:
+                    // messagge  ?
+                    break;
+            }
+        }
+
+
+        private void AddEntity() { 
+        
+            //TODO: OPEN FORM BY ENTITY TYPE (COURSE, STUDENT, PROFESSOR) 
+        
+        }
+
+        private void MdiMainForm_Load(object sender, EventArgs e) {
+            DeserializeFromJson();
+        }
+
+        private void SerializeToJson() {
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            // TODO: SERIALIZE UNIVERSITY OBJECT INSTEAD OF STUDENTS!
+            string data = serializer.Serialize(CodingSchool);
+
+            string path = Path.Combine(Environment.CurrentDirectory, _JsonFile);
+            File.WriteAllText(path, data);
+
+        }
+
+
+        private void DeserializeFromJson() {
+            try {
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+                string path = Path.Combine(Environment.CurrentDirectory, _JsonFile);
+
+                if (File.Exists(path)) {
+                    string data = File.ReadAllText(path);
+
+                    CodingSchool = serializer.Deserialize<University>(data);
+                }
+
+
+            }
+            catch (Exception ex) {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void viewToolStripMenuItem1_Click(object sender, EventArgs e) {
+            ViewForm viewForm = new ViewForm();
+            viewForm.MdiParent = this;
+
+
+            viewForm.ViewData = GetStudentList();
+
+            viewForm.Show();
+        }
+
+
+        private List<string> GetStudentList() {
+
+            List<string> studentList = new List<string>(); 
+
+            try {
+
+                if (CodingSchool?.Students != null) { // != null && CodingSchool.Students != null) {
+
+       
+                    foreach (Student item in CodingSchool.Students) {
+
+                        studentList.Add(string.Format("Name={0} \t Surname={1} \t RegistrationNumber={2}", item.Name, item.Surname, item.RegistrationNumber));
+                    }
+                }
+                else {
+                    MessageBox.Show("No student data exists!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Something wrong happened! Please send me the log file!", "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+                //MessageBox.Show(ex.ToString(), "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                string path = Path.Combine(Environment.CurrentDirectory, _LogFile);
+                File.WriteAllText(path, ex.ToString());
+            }
+
+            return studentList;
+        }
+
+
+        private List<string> GetCoursesList() {
+
+            List<string> coursesList = new List<string>();
+
+            try {
+
+                if (CodingSchool?.Students != null) { // != null && CodingSchool.Students != null) {
+
+
+                    foreach (Course item in CodingSchool.Courses) {
+
+                        coursesList.Add(string.Format("Code={0} \t Subject={1} ", item.Code, item.Subject));
+                    }
+                }
+                else {
+                    MessageBox.Show("No course data exists!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Something wrong happened! Please send me the log file!", "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+                //MessageBox.Show(ex.ToString(), "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                string path = Path.Combine(Environment.CurrentDirectory, _LogFile);
+                File.WriteAllText(path, ex.ToString());
+            }
+
+            return coursesList;
+        }
+
+        private void newCourseToolStripMenuItem_Click(object sender, EventArgs e) {
+            DeserializeFromJson();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+            SerializeToJson();
+        }
+
+        private void addToolStripMenuItem1_Click(object sender, EventArgs e) {
+            AddCourse();
+        }
+
+        private void viewToolStripMenuItem3_Click(object sender, EventArgs e) {
+            ViewForm viewForm = new ViewForm();
+            viewForm.MdiParent = this;
+
+
+            viewForm.ViewData = GetCoursesList();
+
+            viewForm.Show();
+        }
+    }
+}
